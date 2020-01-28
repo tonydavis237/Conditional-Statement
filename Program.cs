4266fc38f3efb78e79b2c231d13ba4400f0de4bd
Tony Davis <anthony79@mail.usf.edu>
2020-01-28 17:55:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+/*
+ Author: Anthony Davis
+ Date: 1/28/2020
+ Description: Project that uses conditional statements once obtaining information from the user
+ */
+
+namespace Conditional_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for the grade they expect to get in the course
+            Console.WriteLine("Please enter the grade that you expect to get in ISM 4300...");
+
+            /* Use try catch statements to vslidate the users information and display an error 
+               message if incorrect information is inputed
+            */
+            try
+            {
+
+            }
+        }
+    }
+}
