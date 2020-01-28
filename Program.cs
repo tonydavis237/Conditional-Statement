1220a2cbd069ee2e26fa4207423db82c0ad66936
Tony Davis <anthony79@mail.usf.edu>
2020-01-28 18:56:22 -0500

    Commit if else if change

--- Program.cs
+++ Program.cs
@@ -19,8 +19,86 @@ namespace Conditional_Statement
             */
             try
             {
+                // Create a variable gathered from input
+                string input = Console.ReadLine();
 
-            }
-        }
-    }
-}
+                // Convert input into interger
+                int grade = int.Parse(input);
+
+                // Use if else if statements to test the inputs correct letter grades from course syllabus
+                if ((grade <= 100) && (grade >= 98))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is A+");
+                }
+                else if ((grade <= 97) && (grade >= 92))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is A");
+                }
+                else if ((grade <= 91) && (grade >= 90))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is A-");
+                }
+                else if ((grade <= 89) && (grade >= 88))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is B+");
+                }
+                else if ((grade <= 87) && (grade >= 82))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is B");
+                }
+                else if ((grade <= 81) && (grade >= 80))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is B-");
+                }
+                else if ((grade <= 79) && (grade >= 78))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is C+");
+                }
+                else if ((grade <= 77) && (grade >= 72))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is C");
+                }
+                else if ((grade <= 71) && (grade >= 70))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is C-");
+                }
+                else if ((grade <= 69) && (grade >= 68))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is D+");
+                }
+                else if ((grade <= 67) && (grade >= 62))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is D");
+                }
+                else if ((grade <= 61) && (grade >= 60))
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is D-");
+                }
+                else
+                {
+                    // Display the correct letter grade
+                    Console.WriteLine("Your lettergrade based from the syllabus is F");
+                }             
+            } // End of try
+            catch
+            {
+                Console.WriteLine("You inputted the incorrect data type :(");
+                Console.WriteLine("Please use an integer data type for the grade you are expecting");
+                Console.WriteLine("Press any key to exit the program and try again...");
+                Console.ReadKey(true);
+            } // End of catch
+        } // End of Main
+    } // End of class
+} // End of namespace
